58a28d9ed4e8914211ae81f3958774f8b69856c8
victor-prm <victorpabb@gmail.com>
2025-02-06 13:00:47 +0100

    updated pickRandomImg function

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 fetch("https://icanhazdadjoke.com/", {
     headers:{
-        "Accept": "application/json"
+        Accept: "application/json"
     } 
 })
 .then(response => response.json())
 .then(data => {
-    console.log(data)
+    pickRandomImg();
+    document.querySelector(".display-joke").innerHTML = data.joke;
+    document.querySelector(".display-id").innerHTML = `ID: #${data.id}`;
+
 })
+
+function pickRandomImg(){
+    let imgElm = document.createElement("img")
+    imgElm.classList.add("display-img")
+    
+    let imgs = ["1.webp","2.jpg","3.jpg","4.webp","5.png","6.jpg","7.jpeg","8.png","9.png","10.jpg","11.jpg"];
+    let randNumb = Math.floor(Math.random()*imgs.length);
+    let path = `./assets/${imgs[randNumb]}`;
+    imgElm.src = path;
+
+    document.querySelector(".img-container").append(imgElm);
+}
